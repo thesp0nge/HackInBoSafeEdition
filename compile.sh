89d28d92f60676856e20d3cc7e02a97fd8a540b9
Paolo Perego <paolo@codiceinsicuro.it>
2020-05-21 20:00:52 +0200

    Helper per la compilazione

---
+++ compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This is the compile helper. I modified a bit, just
+# to detect the file extension and call nasm or gcc
+# accordingly.
+
+dirname=$(dirname -- "$1")
+filename=$(basename -- "$1")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+echo "[*] Compiling $filename"
+
+if [ "$extension" = "c" ]; then
+	echo "[+] Calling gcc without stack protection"
+	gcc -fno-stack-protector -z execstack $1 -o $dirname/$filename
+fi
+
+if [ "$extension" = "nasm" ]; then
+
+	echo '[+] Assembling with Nasm ... '
+	nasm -f elf32 -o $dirname/$filename.o $1
+
+	echo '[+] Linking ...'
+	ld -o $dirname/$filename $dirname/$filename.o
+
+	echo '[+] Done!'
+fi
