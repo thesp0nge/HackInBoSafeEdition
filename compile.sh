a2d0e9e2362cec02fa90b21b9cf7cbe6edc7481d
Paolo Perego <paolo@codiceinsicuro.it>
2020-05-24 10:38:35 +0200

    Modificata opzione a ld

--- compile.sh
+++ compile.sh
@@ -22,7 +22,7 @@ if [ "$extension" = "nasm" ]; then
 	nasm -f elf32 -o $dirname/$filename.o $1
 
 	echo '[+] Linking ...'
-	ld -o $dirname/$filename $dirname/$filename.o
+	ld -m elf_i386 -o $dirname/$filename $dirname/$filename.o
 
 	echo '[+] Done!'
 fi
